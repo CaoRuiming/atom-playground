e772fd1b45189e5144b7e55726b75fcbb1339bfe
CaoRuiming <rcao22556@gmail.com>
2017-12-02 19:35:31 -0500

    first commit

---
+++ spec/atom-playground-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPlaygroundView from '../lib/atom-playground-view';
+
+describe('AtomPlaygroundView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
