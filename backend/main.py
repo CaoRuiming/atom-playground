4ad0f3cc33d7a600bb756ba0ba56959f9e76c707
DorianGregory <dorian@Jellyfish.local>
2017-12-02 23:14:21 -0500

    Added if statement handling to interpreter and abstracted interpreter

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,19 @@
 import sys
 from interpreter import *
 
+# get file name from arguments, execute, and print
 def main():
 	if len(sys.argv) < 2:
 		print("Missing file name")
 	file_name = sys.argv[1]
 	lines = file_to_lines(file_name)
-	counter = 1
-	for line in lines:
-		aug_line = AugmentedLine(counter, line, local_sandbox)
-		aug_line.parse()
-		aug_line.evaluate()
-		print(aug_line.result)
-		counter += 1
+	interpreter = AugmentedInterpreter(lines)
+	interpreter.parse()
+	interpreter.run()
+	interpreter.print_results()
 	print("done.")
 
+# read file name into returned array
 def file_to_lines(file_name):
 	file = open(file_name, 'r')
 	lines = file.readlines()
